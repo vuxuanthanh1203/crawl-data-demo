b718d7a2b422ba380a8b10fdc67a2cd9718abb7a
vuxuanthanh1203 <vuxuanthanh1203@gmail.com>
2023-11-02 23:40:59 +0700

    crawl data for 4 categories

--- scrapeController.js
+++ scrapeController.js
@@ -1,11 +1,45 @@
-const scraper = require('./scraper')
+const scrapers = require('./scraper')
+const fs = require('fs')
 
 const scrapeController = async (browserInstance) => {
     const url = 'https://phongtro123.com/'
+    const indexs = [1, 2, 3, 4]
     try {
-        let browser = await browserInstance
+        const browser = await browserInstance
         // Gọi Scraper
-        let categories = scraper.scrapeCategory(browser, url)
+        const categories = await scrapers.scrapeCategory(browser, url)
+        const selectedCategories = categories.filter((category, index) => indexs.some(i => i === index))
+
+        const result1 = await scrapers.scraper(browser, selectedCategories[0].link)
+        fs.writeFile('chothuephongtro.json', JSON.stringify(result1), 'utf8', (err) => {
+            if (err) {
+                console.log('Ghi data thất bại' + err)
+            }
+            console.log('Ghi data thành công');
+        })
+        const result2 = await scrapers.scraper(browser, selectedCategories[1].link)
+        fs.writeFile('nhachothue.json', JSON.stringify(result2), 'utf8', (err) => {
+            if (err) {
+                console.log('Ghi data thất bại' + err)
+            }
+            console.log('Ghi data thành công');
+        })
+        const result3 = await scrapers.scraper(browser, selectedCategories[2].link)
+        fs.writeFile('chothuecanho.json', JSON.stringify(result3), 'utf8', (err) => {
+            if (err) {
+                console.log('Ghi data thất bại' + err)
+            }
+            console.log('Ghi data thành công');
+        })
+        const result4 = await scrapers.scraper(browser, selectedCategories[3].link)
+        fs.writeFile('chothuematbang.json', JSON.stringify(result4), 'utf8', (err) => {
+            if (err) {
+                console.log('Ghi data thất bại' + err)
+            }
+            console.log('Ghi data thành công');
+        })
+        await browser.close()
+
     } catch (error) {
         console.log("Controller Error: " + error);
     }
